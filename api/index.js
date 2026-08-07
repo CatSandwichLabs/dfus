86994614053c3a9462a5f0a164c0a701759b71c0
aviral-bajpai-official <catsandwichofficial@gmail.com>
2026-08-07 21:35:33 +0530

    chore: add startup error catcher to api/index.js for debugging Vercel 500s

--- api/index.js
+++ api/index.js
@@ -1,2 +1,19 @@
-const app = require('../src/master/server');
+let app;
+try {
+  app = require('../src/master/server');
+} catch (err) {
+  // If the server fails to load, create a dummy express app that just returns the error
+  const express = require('express');
+  app = express();
+  app.use((req, res) => {
+    res.status(500).json({
+      error: 'Failed to initialize server',
+      message: err.message,
+      stack: err.stack,
+      env: process.env.NODE_ENV,
+      mode: process.env.MODE
+    });
+  });
+}
+
 module.exports = app;
